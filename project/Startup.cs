d3defd94f18f747e78b73109556b6f4c7cf13511
unplaged <unplag123@gmail.com>
2020-12-28 20:19:33 +0300

    added colors and jpgs

--- project/Startup.cs
+++ project/Startup.cs
@@ -23,8 +23,8 @@ namespace project
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
